2a6e286c9c7eb263436d064c24776bdde79ce7b1
don-nick1201 <don-nick@inbox.ru>
2023-02-17 15:53:45 +0300

    Закончил разметку HTML

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const sourcemap = require("gulp-sourcemaps");
+const less = require("gulp-less");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sync = require("browser-sync").create();
+
+// Styles
+
+const styles = () => {
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
+    .pipe(sync.stream());
+}
+
+exports.styles = styles;
+
+
+//build
+
+const build = gulp.series(
+  styles
+);
+
+exports.build = build;
+
+
+// Server
+
+const server = (done) => {
+  sync.init({
+    server: {
+      baseDir: 'build'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/less/**/*.less", styles);
+  gulp.watch("source/js/script.js", scripts);
+  gulp.watch("source/*.html", html);
+  done();
+}
+
+exports.server = server;
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch("source/less/**/*.less", gulp.series("styles"));
+  gulp.watch("source/*.html").on("change", sync.reload);
+}
+
+exports.default = gulp.series(
+  styles, watcher
+);
