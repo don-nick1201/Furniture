f12b6fcd5aba6157ae5e595cc70d5be668a5c376
don-nick1201 <don_nick@inbox.ru>
2023-07-06 18:20:25 +0300

    Закончил слайдер главных товаров

--- source/js/script.js
+++ source/js/script.js
@@ -8,3 +8,17 @@ const swiper = new Swiper(".reviews", {
       el: ".reviews__pagination",
     },
   });
+  const swiper1 = new Swiper(".prima-fitch__slider", {
+    // direction: "vertical",
+    centeredSlides: true,
+    pagination: {
+      el: ".prima-fitch__pagination",
+      clickable: true,
+      bulletClass: "prima-fitch__pagination-bullet",
+      bulletActiveClass: "prima-fitch__pagination-bullet--active",
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">0' + (index + 1) + "</span>";
+      },
+    },
+    });
+  
